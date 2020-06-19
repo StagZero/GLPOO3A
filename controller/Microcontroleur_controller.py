36ea31f933d0345da3388ce3f537fca0fe460ab4
keolys <65890166+keolys@users.noreply.github.com>
2020-06-19 20:53:31 +0200

    Faire le contrôleur du Microcontrôleur

--- controller/Microcontroleur_controller.py
+++ controller/Microcontroleur_controller.py
@@ -1,8 +1,10 @@
 import re
+import time
 
 from model.dao.Microcontroleur_dao import MicrocontroleurDAO
 from exceptions import Error, InvalidData
-
+from controller.Bras_controller import BrasController as B
+from controller.Moteur_controller import MoteurController as M
 
 class MicrocontroleurController:
     """
@@ -60,3 +62,52 @@ class MicrocontroleurController:
             Microcontroleur_dao = MicrocontroleurDAO(session)
             Microcontroleur = Microcontroleur_dao.get_by_NumSerie(MC_NumSerie)
             return Microcontroleur.to_dict()
+    
+    
+        
+    def get_Etat(self, MC_id):
+        with self._database_engine.new_session() as session:
+           Microcontroleur = MicrocontroleurDAO(session).get(MC_id)
+        return Microcontroleur['MC_Etat']
+    
+    def attendre_3s(self):
+        
+        time.sleep(3)
+        
+    def attendre_30s(self):
+        
+        time.sleep(30)
+        
+    def start(self, MC_id):
+        with self._database_engine.new_session() as session:
+            MC = MicrocontroleurDAO(session).get(MC_id)
+            B.serrer(MC['B_ID'])
+            
+            data1 = dict(MC_Etat='A')
+            self.update_Microcontroleur(MC_id, data1)
+            
+            self.attendre_3s()
+            M.droite(MC['M_ID'])
+            
+            data2 = dict(MC_Etat='AB')
+            self.update_Microcontroleur(MC_id, data2)
+            
+            self.attendre_30s()
+            M.arreter(MC['M_ID'])
+            B.lacher(MC['B_ID'])
+            
+            data3 = dict(MC_Etat='B')
+            self.update_Microcontroleur(MC_id, data3)
+            
+            self.attendre_3s()
+            M.gauche(MC['M_ID'])
+            
+            data4 = dict(MC_Etat='BS')
+            self.update_Microcontroleur(MC_id, data4)
+            
+            self.attendre_30s()
+            M.arreter(MC['M_ID'])
+    
+            data5 = dict(MC_Etat='S')
+            self.update_Microcontroleur(MC_id, data5)
+        
